a270d651405bc31affc2708edc60f81bc50c5b43
joaovicthor3 <joaovicthor3@gmail.com>
2020-09-01 13:35:53 -0300

    fixing mispellings

--- server.js
+++ server.js
@@ -27,26 +27,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(morgan('tiny'));
 
 // Routes
-app.use('/', require('./routes/api'));
-// app.get('/api', (req, res) => {
-//   const data = {
-//     username: 'testcordeiro',
-//     age: 10
-//   }
-//   res.json(data);
-// });
-//
-// app.get('/api/name', (req, res) => {
-//   const data = {
-//     username: 'tests cordeiroworks',
-//     age: 6
-//   }
-//   res.json(data);
-// });
+app.use('/api', require('./routes/api'));
+
 
 //Step 3
 if (process.env.NODE_ENV === 'production'){
-  app.use(express.static('client/buid'));
+  app.use(express.static('client/build'));
 }
 
 app.listen(PORT, console.log(`Server is starting at ${PORT}`));
